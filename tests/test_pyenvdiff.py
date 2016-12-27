d704e7a0b72a548b762ad04b835ea2c88aeab6af
jnmclarty <jeffrey.mclarty@gmail.com>
2016-12-27 09:06:29 -0500

    Add tests for POST

--- tests/test_pyenvdiff.py
+++ tests/test_pyenvdiff.py
@@ -8,12 +8,34 @@ test_pyenvdiff
 Tests for `pyenvdiff` module.
 """
 
+import mock
 import pytest as pt
 import inspect
+import sys
 
 import pyenvdiff
 from pyenvdiff.collectors import Collector, collector_classes
 from pyenvdiff.info import Environment
+from pyenvdiff.post import get_available_parser_name_and_class, execute_parsing_engine, send
+
+class PythonVersion(object):
+    def __init__(self):
+        pass
+    def __getitem__(self, key):
+        parts = list(map(int, key.split('.')))
+        for i, version_part in enumerate(parts):
+            if version_part != sys.version_info[i]:
+                return False
+        return True
+
+python_version = PythonVersion()
+
+orig_import = __import__
+
+if python_version['2']:
+    builtin = '__builtin__.__import__'
+else:
+    builtin = 'builtins.__import__'
 
 class TestCollectors(object):
     def test_all_collectors_are_captured(self):
@@ -68,3 +90,61 @@ class TestEnvironment(object):
         env_keys = list(self.env.collectors.keys())
         for env_key in env_keys:
             assert len(str(self.env.collectors[env_key])) > 1
+
+class TestPost(object):
+    @classmethod
+    def setup_class(cls):
+        cls.env = Environment()
+
+    def test_get_available_parser_name_and_class(self):
+        parser_name, Parser = get_available_parser_name_and_class()
+        assert parser_name in ('argparse', 'optparse', 'getopt')
+
+    """ TODO: Figure out how to properly test this
+    def test_execute_parsing_engine(self):
+        parser_details = get_available_parser_name_and_class()
+        args = execute_parsing_engine(*parser_details)
+        print(args)
+    """
+
+    def test_getting_optparse(self):
+
+        def import_mock(name, *args):
+            if name == 'argparse':
+                raise ImportError("Argparse not found")
+            return orig_import(name, *args)
+
+        with mock.patch(builtin, side_effect=import_mock):
+            name, Cls = get_available_parser_name_and_class()
+            assert name == 'optparse'
+
+    def test_getting_getopt(self):
+
+        def import_mock(name, *args):
+            if name in ('optparse', 'argparse'):
+                raise ImportError("Latest parsers not found")
+            return orig_import(name, *args)
+
+        with mock.patch(builtin, side_effect=import_mock):
+            name, Cls = get_available_parser_name_and_class()
+            assert name == 'getopt'
+
+    @mock.patch('pyenvdiff.post.Request')
+    @mock.patch('pyenvdiff.post.urlopen')
+    def test_send_basic(self, urlopen, Request):
+
+        class FakeFileStream(object):
+            def read(self):
+                data = '{"result" : "OK", "sha" : "123"}'
+                if python_version['3']:
+                    return data.encode('utf-8')
+                return data
+
+            def close(self):
+                return True
+
+        Request.return_value = True
+        urlopen.return_value = FakeFileStream()
+
+        msg = send(self.env)
+        assert "123" in msg
